b9dc9f2443ece2eb81f4822c1f94ac3216abdd89
Vinicius <silvaoliveirav227@gmail.com>
2026-02-24 23:10:25 -0300

    atualização

--- Negociacao_valores/Negociacao_valores.py
+++ Negociacao_valores/Negociacao_valores.py
@@ -2,7 +2,7 @@ nome = input('Digite o nome do cliente: ')
 CPF = input('Digite o CPF do cliente: ')
 CPF_LIMPO = CPF.replace('.', '').replace('-', '')
 CPF = int(CPF_LIMPO)
-Valor_da_copmra = float(input('Digite o valor da compra: '))
+Valor_da_compra = float(input('Digite o valor da compra: '))
 
 database = {
     36925814700:{
@@ -23,16 +23,16 @@ if CPF in database:
     print(f'Bem-vindo, {database[CPF]["nome"]}!')
 
     if database[CPF]['Categoria'] == 'vip':
-        Desconto = Valor_da_copmra * 0.20
-        Valor_final = Valor_da_copmra - Desconto
+        Desconto = Valor_da_compra * 0.20
+        Valor_final = Valor_da_compra - Desconto
         print(f'O valor final da compra é: R${Valor_final:.2f}')    
 
     elif database[CPF]['Categoria'] == 'premium':
-        Desconto = Valor_da_copmra * 0.10
-        Valor_final = Valor_da_copmra - Desconto
+        Desconto = Valor_da_compra * 0.10
+        Valor_final = Valor_da_compra - Desconto
         print(f'O valor final da compra é: R${Valor_final:.2f}')
     else:
-        print(f'O valor final da compra é: R${Valor_da_copmra:.2f}')
+        print(f'O valor final da compra é: R${Valor_da_compra:.2f}')
 
 else:
     Cliente = input('Digite a Categoria (vip,premium,comum)').lower()
@@ -42,23 +42,23 @@ else:
         resposta = input('Deseja se cadastrar para obter descontos exclusivos? (sim/não)').lower()
 
         if resposta == 'sim':
-            Desconto = Valor_da_copmra * 0.05
-            Valor_final = Valor_da_copmra - Desconto
+            Desconto = Valor_da_compra * 0.05
+            Valor_final = Valor_da_compra - Desconto
             print(f'Cadastro realizado com sucesso! Bem-vindo, {nome}! O valor final da compra é: R${Valor_final:.2f}')    
         elif resposta == 'não': 
-            print(f'Entendido, {nome}. O valor final da compra é: R${Valor_da_copmra:.2f}')
+            print(f'Entendido, {nome}. O valor final da compra é: R${Valor_da_compra:.2f}')
                 
         else:
             print('Resposta inválida. Por favor, responda com "sim" ou "não".')
 
     elif database[CPF]['Categoria'] == 'vip':
-        Desconto = Valor_da_copmra * 0.20
-        Valor_final = Valor_da_copmra - Desconto
+        Desconto = Valor_da_compra * 0.20
+        Valor_final = Valor_da_compra - Desconto
         print(f'O valor final da compra é: R${Valor_final:.2f}')    
 
     elif database[CPF]['Categoria'] == 'premium':
-        Desconto = Valor_da_copmra * 0.10
-        Valor_final = Valor_da_copmra - Desconto
+        Desconto = Valor_da_compra * 0.10
+        Valor_final = Valor_da_compra - Desconto
         print(f'O valor final da compra é: R${Valor_final:.2f}')
     else:
         print(f'Invalido, por favor, escolha entre vip, premium ou comum.')
